543136f437917109638bc168cbd4f9bb87082399
artisticoinformatico <96482901+artisticoinformatico@users.noreply.github.com>
2022-03-26 20:24:33 +0100

    first commit

---
+++ clicker.js
@@ -0,0 +1,34 @@
+let pkt=0.0; //obecnie
+let wszystkie=0.0;
+let klikniecia=0;
+let wartosc=1;
+let lvl=1;
+
+if (localStorage.pkt && localStorage.wszystkie && localStorage.klikniecia) {
+  pkt = parseFloat(localStorage.pkt);
+  wszystkie = parseFloat(localStorage.wszystkie);
+  klikniecia = parseInt(localStorage.klikniecia);
+  document.getElementById("pkt_teraz").innerHTML=parseInt(pkt);
+}
+
+if (localStorage.lvl) {
+  lvl = parseInt(localStorage.lvl);
+}
+
+
+
+
+
+function lewyxd() {
+  pkt+=wartosc;
+  document.getElementById("pkt_teraz").innerHTML=parseInt(pkt);
+
+
+  wszystkie+=wartosc; //staty
+  klikniecia++;
+
+  localStorage.pkt = pkt; //save
+  localStorage.klikniecia = klikniecia;
+  localStorage.wszystkie = wszystkie;
+
+}
