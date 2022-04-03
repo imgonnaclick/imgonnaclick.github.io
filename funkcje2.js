6c0aef6d5df49dbc16b5d1be8fdc121cd8992622
artisticoinformatico <96482901+artisticoinformatico@users.noreply.github.com>
2022-04-03 18:13:21 +0200

    fix komunikat

--- funkcje2.js
+++ funkcje2.js
@@ -1,10 +1,10 @@
+let ifdone = 0;
 function komunikat() {
-  let ifdone = 0;
   let idkomunikatu = document.getElementById("idkomunikatu");
   idkomunikatu.className = "komunikat komunikat_on";
   setTimeout(function(){
     idkomunikatu.className = "komunikat";
   },2000);
-  let ifdone = 1;
+  ifdone = 1;
 
 }
